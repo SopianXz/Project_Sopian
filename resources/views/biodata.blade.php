33b9796a9d826d4fb43f06c3b57c3d0a6ebbe628
SopianXz <sopiannurdiansyah605@gmail.com>
2024-04-25 08:28:43 +0700

    Push Yang Pertama

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        selamat datang di route biodata
+    </h1>
+    Nama : {{ $nama }} <br>
+    jenis kelamin : {{ $Jk }} <br>
+    alamat : {{ $alamat }} <br>
+    pendidikan : {{ $pendidikan }} <br>
+    pekerjaan : {{ $pekerjaan }} <br>
+
+</body>
+
+</html>
---
+++ resources/views/param.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Selamat datang di halaman parameter</h1>
+
+    Nama : {{ $nama }} <br>
+    jenis kelamin : {{ $Jk }} <br>
+    alamat : {{ $alamat }} <br>
+    pendidikan : {{ $pendidikan }} <br>
+    pekerjaan : {{ $pekerjaan }} <br>
+</body>
+
+</html>
